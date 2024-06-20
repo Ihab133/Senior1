99cfb21df5eecb32c53ba0f57231cb51e62ffe45
Ihab Ahmad <122423479+Ihab133@users.noreply.github.com>
2024-06-20 17:06:18 +0300

    Update auth.route.js

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { login, logout, register } from "../controllers/auth.controller.js";
+import { login , logout, register, userInfo } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
+router.get("/userInfo/:id", userInfo);
+
 
 export default router;
