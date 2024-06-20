ef61d6d91ad6e6063beb635dd2aed1eabb1cc42c
Ihab Ahmad <122423479+Ihab133@users.noreply.github.com>
2024-06-20 17:07:35 +0300

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,19 +1,27 @@
 import express from "express";
+import mysql from "mysql";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
-import postRoute from "./routes/post.route.js";
-
+import authRoute from "./routes/auth.route.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));
+app.use(cors({
+    origin : ["http://localhost:5173"] , methods: ["POST" , "GET"] , credentials: true
+}));
 app.use(cookieParser());
 
-app.use("/api/posts", postRoute);
+
 app.use("/api/auth", authRoute);
 
-app.listen(8800, () => {
-    console.log("Server is Running");
+
+
+
+
+
+
+
+app.listen(3001, () => {
+    console.log("Server Is Running");
 });
